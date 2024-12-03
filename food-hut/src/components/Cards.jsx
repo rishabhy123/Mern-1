476e78de402577c26760e84c7e4a4009cce6a0fc
rishabhy123 <www.rishabhyadav2004@gmail.com>
2024-12-03 23:44:52 +0530

    Updated responsive design for cards and added 'Add to Cart' functionality

--- food-hut/src/components/Cards.jsx
+++ food-hut/src/components/Cards.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react';
+import  { useEffect, useRef, useState } from 'react';
 import { useCartDispatch, useCartState } from './ContextReducer';
-
+import './Cards.css'
 export default function Cards(props) {
   let options = props.options;
   let dispatch = useCartDispatch();
@@ -12,7 +12,6 @@ export default function Cards(props) {
   const [size, setSize] = useState("");
 
   const handleAddtoCart = async () => {
-
     let food = [];
     for (const item of data) {
       if (item.id === props.foodItem._id) {
@@ -28,7 +27,7 @@ export default function Cards(props) {
           price: finalPrice,
           qty: qty
         });
-        return
+        return;
       }
       else if (food.size !== size) {
         await dispatch({
@@ -39,9 +38,9 @@ export default function Cards(props) {
           qty: qty,
           size: size
         });
-        return
+        return;
       }
-      return
+      return;
     }
     await dispatch({
       type: "ADD_ITEM",
@@ -52,33 +51,35 @@ export default function Cards(props) {
       size: size
     });
   }
+  
   let finalPrice = qty * parseInt(options[size]);
+
   useEffect(() => {
-    setSize(priceRef.current.value)
-  }, [])
+    setSize(priceRef.current.value);
+  }, []);
 
   return (
-    <div>
-      <div className="card mt-3" style={{ width: "20vw", maxHeight: "420px" }}>
-        <img className="card-img-top" src={props.foodItem.img} style={{ height: "150px", objectFit: "fill" }} alt={props.foodItem.name} />
+    <div className="card-container">
+      <div className="card mt-3">
+        <img className="card-img-top" src={props.foodItem.img} alt={props.foodItem.name} />
         <div className="card-body">
           <h5 className="card-title">{props.foodItem.name}</h5>
           <p className="card-text">{props.descriptionFood}</p>
           <div className="container w-100">
-            <select className='h-100 m-2 bg-success rounded' onChange={(e) => setQty(e.target.value)}>
+            <select className='quantity-selector' onChange={(e) => setQty(e.target.value)}>
               {Array.from(Array(6), (e, i) => (
                 <option key={i + 1} value={i + 1}>{i + 1}</option>
               ))}
             </select>
-            <select className='m-2 h-100 rounded bg-success' ref={priceRef} onChange={(e) => setSize(e.target.value)}>
+            <select className='size-selector' ref={priceRef} onChange={(e) => setSize(e.target.value)}>
               {priceOptions.map((data) => (
                 <option key={data} value={data}>{data}</option>
               ))}
             </select>
-            <div className='fs-5 d-inline'>Rs.{finalPrice}/-</div>
+            <div className='price-display'>Rs.{finalPrice}/-</div>
           </div>
           <hr />
-          <button className='btn btn-success justify-center ms-5' onClick={handleAddtoCart}>Add to Cart</button>
+          <button className='add-to-cart-btn' onClick={handleAddtoCart}>Add to Cart</button>
         </div>
       </div>
     </div>
